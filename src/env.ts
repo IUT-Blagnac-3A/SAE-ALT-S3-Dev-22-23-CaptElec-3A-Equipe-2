8ec7bce56412eea0774cbabc5a855ccb366db264
Eric <ericphlpp@gmail.com>
2023-01-17 10:20:39 +0100

    Push

--- src/env.ts
+++ src/env.ts
@@ -1,11 +1,7 @@
 require("dotenv").config();
 
 const UNDEFINED = "UNDEFINED";
-const EMAIL = process.env["EMAIL"] || UNDEFINED;
-const EMAIL_PASSWORD = process.env["EMAIL_PASSWORD"] || UNDEFINED;
 
 module.exports = {
   UNDEFINED,
-  EMAIL,
-  EMAIL_PASSWORD,
 };
