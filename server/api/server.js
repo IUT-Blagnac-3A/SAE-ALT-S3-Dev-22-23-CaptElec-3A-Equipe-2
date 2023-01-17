a3f0b432d02c570a7927de1a70edc79ea4724dd9
Matthieu Robert <matthieurobert82@gmail.com>
2023-01-17 11:40:48 +0100

    Create docker config for api and mqtt service

--- server/api/server.js
+++ server/api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 // const bodyParser = require('body-parser');
 const app = express();
-const db = require('../mqtt/db');
+const db = require('./db');
 
 app.get('/', (req, res) => {
     res.send('Hello world')
