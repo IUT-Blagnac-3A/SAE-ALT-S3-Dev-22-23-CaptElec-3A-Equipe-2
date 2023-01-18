8243b7f3990c4558e98664f5e02acb329108b84b
CashMTS <mathis.mrc31@gmail.com>
2023-01-18 09:02:07 +0100

    added header

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FooterComponent } from "./footer/footer.component";
 import { HttpClientModule } from "@angular/common/http";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent],
+  declarations: [AppComponent, FooterComponent, HeaderComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   bootstrap: [AppComponent],
 })
