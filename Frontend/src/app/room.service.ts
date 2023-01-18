4a6cad514366b4770b6c4e5a7d700870766bcba5
CashMTS <mathis.mrc31@gmail.com>
2023-01-18 10:51:49 +0100

    created Room Object and Room service

---
+++ Frontend/src/app/room.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Room } from './room';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomService {
+
+  constructor(private http: HttpClient) {}
+
+  public getRooms(): Observable<Room[]>{
+    return this.http.get<any>('localhost:3000/data');
+  }
+
+}
