22f38991aef5adee1f0871bb19c2163847c749bd
Matthieu Robert <matthieurobert82@gmail.com>
2023-01-18 10:02:23 +0100

    migrate cors middleware to middleware folder

--- Server/api/src/index.ts
+++ Server/api/src/index.ts
@@ -1,23 +1,11 @@
 import express, { Express } from 'express';
 import { routes } from './routes/route.js';
+import corsMiddleware from './middleware/cors.js';
 
 const app: Express = express();
 
 // Allow any method from any host and log requests
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, DELETE");
-    if ("OPTIONS" === req.method) {
-      res.sendStatus(200);
-    } else {
-      console.log(`${req.ip} ${req.method} ${req.url}`);
-      next();
-    }
-});
+app.use(corsMiddleware);
 
 // Handle POST requests that come in formatted as JSON
 app.use(express.json());
