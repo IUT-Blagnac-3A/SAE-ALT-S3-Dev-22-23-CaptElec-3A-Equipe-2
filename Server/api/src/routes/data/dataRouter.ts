e5e22a12398e1565df8659206eff31c48f7d2e20
Matthieu Robert <matthieurobert82@gmail.com>
2023-01-23 14:47:24 +0100

    Create route to get only one type of data

--- Server/api/src/routes/data/dataRouter.ts
+++ Server/api/src/routes/data/dataRouter.ts
@@ -2,9 +2,11 @@ import * as express from 'express';
 import getData from './getData.js';
 import getDataFromDevice from './getDataByDeviceName.js';
 import getDataByRoom from './getDataByRoom.js';
+import getDataFromTypeHandler from './getDataType.js';
 
 export const dataRouter = express.Router()
 
 dataRouter.get("/", getData)
+dataRouter.get("/type/:type", getDataFromTypeHandler)
 dataRouter.get("/:deviceName", getDataFromDevice)
 dataRouter.get('/:project/:room', getDataByRoom)
