87ca82efb7c358ef7b41a624dc5b2bd887fe5bbc
Matthieu Robert <matthieurobert82@gmail.com>
2023-01-23 10:31:11 +0100

    Remove unused user routes

--- Server/api/src/routes/users/getUsers.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express"
-
-export default function getUsers(req: Request, res: Response) {
-    res.send([])
-}
--- Server/api/src/routes/users/postUser.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response } from "express"
-
-export default function postUser(req: Request, res: Response) {
-    res.send({ body: req.body })
-}
