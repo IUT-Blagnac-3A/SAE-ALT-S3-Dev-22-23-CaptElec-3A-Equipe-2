8e14537b00014370a8f87eef61ff520875e4c1a0
Tamachiii <tamachi.konoyaro@gmail.com>
2023-01-23 15:35:47 +0100

    Add id for device table

--- infra/db/01-init.sh
+++ infra/db/01-init.sh
@@ -17,6 +17,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USERNAME" --dbname "$POSTGRES_DB" 
     );
     CREATE TABLE IF NOT EXISTS device
     (
+      devui VARCHAR(32),
       name_device VARCHAR(32),
       name_room VARCHAR(32),
       ts timestamp with time zone NOT NULL,
@@ -25,7 +26,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USERNAME" --dbname "$POSTGRES_DB" 
       humidity double precision,
       pressure double precision,
       temperature double precision,
-      PRIMARY KEY (name_device),
+      PRIMARY KEY (devui),
       FOREIGN KEY (name_room) REFERENCES room(name_room)
     );
     CREATE TABLE IF NOT EXISTS battery
@@ -35,6 +36,13 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USERNAME" --dbname "$POSTGRES_DB" 
         battery double precision,
         PRIMARY KEY (name_device),
         FOREIGN KEY (name_device) REFERENCES device(name_device)
+    );
+    CREATE TABLE IF NOT EXISTS users
+    (
+        id character varying(32) COLLATE pg_catalog."default",
+        username character varying(32) COLLATE pg_catalog."default",
+        email character varying(252) COLLATE pg_catalog."default",
+        password character varying(252) COLLATE pg_catalog."default"
     );
     COMMIT;
 EOSQL
