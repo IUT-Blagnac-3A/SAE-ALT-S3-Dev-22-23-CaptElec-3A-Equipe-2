302f042e48dfbe5d8c89a4f7e9c2216f6be38767
Tamachiii <tamachi.konoyaro@gmail.com>
2023-01-23 16:23:49 +0100

    Fix typo on insertDatasToProject

--- Server/mqtt/db.js
+++ Server/mqtt/db.js
@@ -2,8 +2,8 @@
 const postgres = require('postgres') 
 
 // Connection to database
-const sql = postgres('postgres://postgres:password@postgres:5432/postgres', {
-    host: process.env.POSTGRES_HOST,      // Postgres ip address[s] or domain name[s]
+const sql = postgres('postgres://postgres:password@localhost:5432/postgres', {
+    host: 'localhost',      // Postgres ip address[s] or domain name[s] // process.env.POSTGRES_HOST
     port: process.env.POSTGRES_PORT,             // Postgres server port[s]
     database: process.env.POSTGRES_DB,  // Name of database to connect to
     username: process.env.POSTGRES_USERNAME,  // Username of database user
@@ -12,9 +12,15 @@ const sql = postgres('postgres://postgres:password@postgres:5432/postgres', {
 
 async function insertData({devEUI, ts, activity, co2, humidity, pressure, temperature }) {
     const res = await sql`
+<<<<<<< HEAD
         INSERT INTO data (devEUI, ts, activity, co2, humidity, pressure, temperature )
         VALUES (${ devEUI }, ${ ts }, ${ activity }, ${ co2 }, ${ humidity }, ${ pressure }, ${ temperature } )
         RETURNING devEUI, ts, activity, co2, humidity, pressure, temperature
+=======
+        INSERT INTO project ( name_project )
+        VALUES ( ${ name_project } )
+        RETURNING name_device
+>>>>>>> Fix typo on insertDatasToProject
     `
     return res
 }
