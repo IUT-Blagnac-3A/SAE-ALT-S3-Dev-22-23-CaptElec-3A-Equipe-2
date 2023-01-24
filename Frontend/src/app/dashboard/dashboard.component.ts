b11fa1b2d8776fefcb46f0dc9a85327e1cb0de7c
CashMTS <mathis.mrc31@gmail.com>
2023-01-24 10:38:19 +0100

    created custom chart plugin

--- Frontend/src/app/dashboard/dashboard.component.ts
+++ Frontend/src/app/dashboard/dashboard.component.ts
@@ -8,6 +8,7 @@ import { RoomService } from "../room.service";
 import { Room } from "../room";
 import { HttpErrorResponse } from "@angular/common/http";
 import { Chart, registerables } from "chart.js";
+
 Chart.register(...registerables);
 
 @Component({
@@ -41,8 +42,8 @@ export class DashboardComponent {
     values.forEach((files: File) => {
       files.displayOnPage();
     });
-    
-    this.renderChart();
+
+    this.renderCharts();
   }
 
   ngAfterViewInit() {
@@ -107,50 +108,107 @@ export class DashboardComponent {
     )
   }
 
-  renderChart(): void{
-    const batteryChart = new Chart("batteryChart", {
-      type: 'bar',
-      data: {
-        labels: ["labels"],
-        datasets: [
-          {
-            label: 'Dataset 1',
-            data: [15],
-            backgroundColor: "red",
-          },
-          {
-            label: 'Dataset 3',
-            data: [100],
-            backgroundColor: "black",
-          }
-        ]},
-      options: {
-        plugins: {
-          tooltip:{
-            enabled: false
-          },
-          legend: {
+  renderCharts(): void {
+
+  // render init block
+  const batteryChart = new Chart("batteryChart", {
+    type: 'bar',
+    data: {
+      labels: [""],
+      datasets: [
+        {
+          label: 'Actual battery',
+          data: [15],
+          backgroundColor: "#dc3545",
+        },
+        {
+          label: 'Maximum battery',
+          data: [100],
+          backgroundColor: "#212529",
+        }
+      ]
+    },
+    options: {
+      plugins: {
+        tooltip: {
+          mode: 'point',
+        },
+        legend: {
+          display: false
+        },
+        title: {
+          display: false,
+          text: 'Below 15% a notification will be sent',
+          position: "bottom"
+        },
+      },
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        x: {
+          display: false,
+          stacked: true,
+        },
+        y: {
+          stacked: true,
+          min: 0,
+          max: 100,
+          grid: {
             display: false
           },
-          title: {
-            display: true,
-            text: 'Below 15% a notification will be sent to you',
-            position: "bottom"
-          },
-        },
-        responsive: true,
-        scales: {
-          x: {
-            display: false,
-            stacked: true,
-          },
-          y: {
-            stacked: true,
-            min:0,
-            max:100
+          ticks: {
+            maxTicksLimit: 6
           }
         }
-      }
+      },
+    }
   });
+
+  const centerText = {
+    id: 'centerText',
+    afterDatasetsDraw(chart: Chart, args: any, pluginOptions: any){
+      const {ctx} = chart;
+
+      const text = "60";
+
+      ctx.save()
+      const x = chart.getDatasetMeta(0).data[0].x;
+      const y = chart.getDatasetMeta(0).data[0].y;
+
+      ctx.textAlign = 'center';
+      ctx.font = "20pt sans-serif";
+
+      ctx.fillText(text,x,y-11)
+    }
   }
+
+  //co2
+  const co2Chart = new Chart("gauge", {
+    type: 'doughnut',
+    data: {
+      labels: ['Mon', 'Tue'],
+      datasets: [{
+        label: 'Weekly Sales',
+        data: [18, 15],
+        backgroundColor: [
+          'rgba(255, 26, 104, 0.2)',
+          'rgba(54, 162, 235, 0.2)'
+        ],
+        borderWidth: 0
+      }]
+    },
+    options: {
+      plugins:{
+        legend: {
+          display: false
+        }
+      },
+      circumference: 180,
+      rotation: -90,
+      cutout: 60,
+    }
+  });
+
+  Chart.register(centerText);
+}
 }
