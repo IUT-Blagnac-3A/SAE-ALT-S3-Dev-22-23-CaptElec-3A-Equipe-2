959a99780c8cccd6006ab0bd98e896f449474986
CashMTS <mathis.mrc31@gmail.com>
2023-01-25 10:59:11 +0100

    added max and critical rates for api's data

--- Frontend/src/app/modules/default.dico.ts
+++ Frontend/src/app/modules/default.dico.ts
@@ -14,4 +14,22 @@ module.exports = class DefaultDico {
   get ACTIVITY_UNIT() {
     return "µg/m³";
   }
+  get CRITICAL_CO2(){
+    return 1000;
+  }
+  get CRITICAL_HUMIDITY(){
+    return 70;
+  }
+  get CRITICAL_TEMPERATURE(){
+    return 40;
+  }
+  get MAX_CO2(){
+    return 5000;
+  }
+  get MAX_HUMIDITY(){
+    return 100;
+  }
+  get MAX_TEMPERATURE(){
+    return 50;
+  }
 };
