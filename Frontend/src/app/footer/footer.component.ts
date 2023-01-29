cd448600db47203d2f36a20ff1163ae80416f98a
CashMTS <mathis.mrc31@gmail.com>
2023-01-29 20:36:29 +0100

    "removed links to change view from footer"

--- Frontend/src/app/footer/footer.component.ts
+++ Frontend/src/app/footer/footer.component.ts
@@ -6,21 +6,6 @@ import { ViewService } from '../view.service';
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit{
-
-  viewService!: ViewService;
-
-  constructor(private viewServ: ViewService){}
-
-  ngOnInit(): void {
-    this.viewService = this.viewServ;
-  }
-
-  public changeView(viewName: string) {
-    this.viewService.resetDashboardId();
-    let b = false;
-    if (viewName === "Dashboard") b = true;
-    this.viewService.setView(viewName, b);
-  }
+export class FooterComponent{
 
 }
