9b47678be542a76b3c9939c1cb7d9ad62667db77
CashMTS <mathis.mrc31@gmail.com>
2023-01-29 20:32:18 +0100

    "changed footer, sidebar and added font"

--- Frontend/src/app/footer/footer.component.ts
+++ Frontend/src/app/footer/footer.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ViewService } from '../view.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit{
+
+  viewService!: ViewService;
+
+  constructor(private viewServ: ViewService){}
+
+  ngOnInit(): void {
+    this.viewService = this.viewServ;
+  }
+
+  public changeView(viewName: string) {
+    this.viewService.resetDashboardId();
+    let b = false;
+    if (viewName === "Dashboard") b = true;
+    this.viewService.setView(viewName, b);
+  }
 
 }
